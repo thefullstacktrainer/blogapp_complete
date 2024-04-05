7c2ce5eb997c13e6bee87480a0623c3262dab038
Lakshmikant <fullstacktrainer4u@gmail.com>
2024-04-05 11:31:03 +0530

    basic root api (GET)

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => res.send("Welcome to blogpost"));
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
