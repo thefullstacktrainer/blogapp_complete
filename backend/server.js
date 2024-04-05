a6be2bbbaf54be14a4951f078b9ff8d79c6f36a6
Lakshmikant <fullstacktrainer4u@gmail.com>
2024-04-05 11:32:06 +0530

    changed port to 4000

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
