c54c59fcdb6b6ba2be99ba1e50ec33a0797e2fd3
“lakshmikant” <fullstacktrainer4u@gmail.com>
2024-04-08 21:26:14 +0530

    changes to support fe

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ connection.connect()
 connection.query(`
     CREATE TABLE IF NOT EXISTS posts (
         post_id SERIAL PRIMARY KEY,
-        post_name VARCHAR(255) NOT NULL,
+        postName VARCHAR(255) NOT NULL,
         description TEXT NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
