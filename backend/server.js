0c4b9ea99e568a8450de5b63b0cabc96483b2e22
unknown <vinaykumarbeelagi@gmail.com>
2024-04-08 19:01:40 +0530

    be_render created and added cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
-
+app.use(cors());
 app.get("/", (req, res) => res.send("Welcome to blogpost"));
 
 let posts = [];
